b27e82325ee9c1b1d1bb40c0b363d2acdf2594f4
JairoTeran <jairo.teran@talosgitial.com>
2021-05-10 16:06:38 -0500

    Setter injector

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,23 @@
+import com.teran.repository.HibernateSpeakerRepositoryImpl;
+import com.teran.repository.SpeakerRepository;
+import com.teran.service.SpeakerService;
+import com.teran.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name="speakerService")
+    public SpeakerService getSpeakerService(){
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository(){
+        return new HibernateSpeakerRepositoryImpl();
+    }
+
+}
