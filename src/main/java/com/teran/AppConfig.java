a3f07706138be82a0155ac86bdcb4b7e5be55a18
JairoTeran <jairo.teran@talosgitial.com>
2021-05-10 16:56:19 -0500

    Constructor injection

--- src/main/java/com/teran/AppConfig.java
+++ src/main/java/com/teran/AppConfig.java
@@ -4,16 +4,19 @@ import com.teran.repository.HibernateSpeakerRepositoryImpl;
 import com.teran.repository.SpeakerRepository;
 import com.teran.service.SpeakerService;
 import com.teran.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name="speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService(){
-        SpeakerServiceImpl service = new SpeakerServiceImpl();
-        service.setRepository(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+
         return service;
     }
 
--- src/main/java/com/teran/service/SpeakerServiceImpl.java
+++ src/main/java/com/teran/service/SpeakerServiceImpl.java
@@ -10,12 +10,13 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        this.repository = speakerRepository;
+    }
+
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
     }
 
-    public void setRepository(SpeakerRepository repository) {
-        this.repository = repository;
-    }
 }
