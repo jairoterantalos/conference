e43828f93f029aa68efe1622d8b6a7b1cc50d206
JairoTeran <jairo.teran@talosgitial.com>
2021-05-11 10:25:28 -0500

    Constructor injector

--- src/main/java/com/teran/service/SpeakerServiceImpl.java
+++ src/main/java/com/teran/service/SpeakerServiceImpl.java
@@ -9,12 +9,13 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
     }
 
-    public void setRepository(SpeakerRepository repository) {
-        this.repository = repository;
-    }
 }
